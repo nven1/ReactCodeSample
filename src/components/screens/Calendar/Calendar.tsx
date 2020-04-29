e10ae6fdb2aa786afba39178bb5efa71417bf636
nven1 <neven.zdelar@gmail.com>
2020-04-29 14:25:15 +0200

    Calendar screen with navigation for subpages, removed redundant files

--- src/components/screens/Calendar/Calendar.tsx
+++ src/components/screens/Calendar/Calendar.tsx
@@ -2,9 +2,12 @@ import React from "react"
 import styles from "./Calendar.module.scss"
 import { RouteComponentProps, useParams, withRouter, Switch, Route, Redirect } from "react-router"
 import Endpoints from "../../../environments/endpoints"
-import CalendarOverview from "../../calendar_components/CalendarOverview/CalendarOverview"
+import CalendarManage from "../../calendar_components/CalendarManage/CalendarManage"
 import CalendarAdd from "../../calendar_components/CalendarAdd/CalendarAdd"
-import Toolbar from "../../common_components/bars/Toolbar/Toolbar"
+import Toolbar, { ToolbarButtonItemType } from "../../common_components/bars/Toolbar/Toolbar"
+import { isNum } from "../../../utils/navHelpers"
+import { useSelector } from "react-redux"
+import { selectIsAdmin } from "../../../reducers/UserReducer"
 
 interface CalendarProps extends RouteComponentProps {}
 
@@ -12,22 +15,29 @@ const URL = Endpoints.appEndpoints.calendar
 
 const Calendar: React.FC<CalendarProps> = (props) => {
     const { mode } = useParams()
+    const isAdmin = useSelector(selectIsAdmin)
 
-    const renderRequest = () => {
-        if (!isNaN(Number(mode))) {
-            return <CalendarOverview />
-        } else {
-            return <Redirect to={URL} />
-        }
-    }
+    const toolbarButtons: Array<ToolbarButtonItemType> = [
+        { key: "me", name: "Me" },
+        { key: "manage", name: "Manage" },
+    ]
+
+    const userTypeHome = isAdmin ? "manage" : "me"
 
     return (
         <div className={styles.container}>
-            <Toolbar url={URL} label="Calendar" actionLabel={mode === "add" ? "" : "Add leave"} onAction="add" />
+            <Toolbar
+                url={URL}
+                label="Calendar"
+                actionLabel={mode === "add" ? "" : "Add leave"}
+                onAction="add"
+                buttons={isAdmin ? toolbarButtons : [toolbarButtons[0]]}
+            />
             <Switch>
-                <Route exact path={URL} component={CalendarOverview} />
+                <Route exact path={`${URL}`} render={() => <Redirect to={`${URL}/${userTypeHome}`} />} />
+                <Route exact path={`${URL}/me`} component={CalendarManage} />
                 <Route exact path={`${URL}/add`} component={CalendarAdd} />
-                <Route path={`${URL}/:mode?`} render={renderRequest} />
+                <Route exact path={`${URL}/manage/:id${isNum}?`} component={CalendarManage} />
             </Switch>
         </div>
     )
