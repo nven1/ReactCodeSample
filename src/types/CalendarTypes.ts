97cc7b64d445a741fded1032a41ee5d7c697cba1
nven1 <neven.zdelar@gmail.com>
2020-04-30 21:16:03 +0200

    Calendar types file

---
+++ src/types/CalendarTypes.ts
@@ -0,0 +1,47 @@
+import { UserVacationType } from "./UserTypes"
+
+export interface VacationType {
+    id: number
+    startingDate: string
+    endingDate: string
+    created: string
+    status: VacationStatusType
+    numberOfVacationDays: number
+    user: UserVacationType
+}
+
+export type VacationStatusType = "APPROVED" | "DECLINED" | "PENDING"
+
+export interface CalendarEvent {
+    id: number
+    title: string
+    start: string
+    end: string
+    extendedProps: UserVacationType
+}
+
+export interface VacationRequestReviewType {
+    id: number
+    title: string
+    start: string
+    end: string
+    user: UserVacationType
+    daysRequested: number
+    status: VacationStatusType
+}
+
+export interface UserDaysLeft {
+    year: number
+    daysLeftToBook: number
+}
+
+export interface CalendarSelectType {
+    start: string
+    end: string
+}
+
+export interface CalendarRequestType {
+    userId: number
+    startingDate: string
+    endingDate: string
+}
