615df3b3b5efe88ebeb6557bfb8e662d60479567
nven1 <neven.zdelar@gmail.com>
2020-04-30 21:03:15 +0200

    Request list and single request view

---
+++ src/components/calendar_components/RequestList/RequestList.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import styles from "./RequestList.module.scss"
+import Card from "../../common_components/containers/Card/Card"
+import Title from "../../common_components/text/Title/Title"
+import { useHistory } from "react-router"
+import { goTo } from "../../../utils/navHelpers"
+import Endpoints from "../../../environments/endpoints"
+import Button from "../../common_components/buttons/Button/Button"
+import { VacationRequestReviewType } from "../../../types/CalendarTypes"
+import Item from "../../common_components/text/Item/Item"
+
+interface RequestListProps {
+    requests: Array<VacationRequestReviewType>
+}
+
+const RequestList: React.FC<RequestListProps> = (props) => {
+    const history = useHistory()
+
+    const handleClick = (id: number) => () => {
+        history.push(goTo(Endpoints.appEndpoints.calendar, "manage", id.toString()))
+    }
+
+    const list = props.requests.map((request) => {
+        return (
+            <Item label={request.title} bold>
+                <Button color="purple" size="small" onClick={handleClick(request.id)}>
+                    Review
+                </Button>
+            </Item>
+        )
+    })
+
+    return (
+        <div className={styles.container}>
+            <Card>
+                <Title>Requests</Title>
+                <div className={styles.content}>{list}</div>
+            </Card>
+        </div>
+    )
+}
+export default RequestList
