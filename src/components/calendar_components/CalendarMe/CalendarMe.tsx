dbd2810d32e31f1614a2a0d7a5af2f6f5af26e6c
nven1 <neven.zdelar@gmail.com>
2020-04-30 21:10:35 +0200

    Calendar current user subscreen

---
+++ src/components/calendar_components/CalendarMe/CalendarMe.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react"
+import styles from "./CalendarMe.module.scss"
+import { useSelector, useDispatch } from "react-redux"
+import { selectMyVacations } from "../../../reducers/CalendarReducer"
+import { selectMe } from "../../../reducers/UserReducer"
+import { VacationRequestReviewType, CalendarEvent } from "../../../types/CalendarTypes"
+import Subtitle from "../../common_components/text/Subtitle/Subtitle"
+import CalendarDataAccess from "../../../data_access/CalendarDataAccess"
+import Card from "../../common_components/containers/Card/Card"
+import CalendarCore from "../CalendarCore/CalendarCore"
+import { formatDate } from "../../../utils/formattingHelpers"
+import Item from "../../common_components/text/Item/Item"
+
+interface CalendarMeProps {}
+
+const CalendarMe: React.FC<CalendarMeProps> = (props) => {
+    const dispatch = useDispatch()
+
+    const me = useSelector(selectMe)
+    const vacations = useSelector(selectMyVacations)
+
+    const [myVacations, setMyVacations] = useState<Array<VacationRequestReviewType>>([])
+    const [daysRemaining, setDaysRemaining] = useState<number>()
+
+    useEffect(() => {
+        if (me) {
+            CalendarDataAccess.getUserDaysRemaining(dispatch)(me.id, onSuccessDaysRemaining)
+            CalendarDataAccess.getMyVacations(dispatch)()
+        }
+
+        // eslint-disable-next-line
+    }, [me])
+
+    useEffect(() => {
+        if (me) {
+            const mine = vacations.filter((vacation) => vacation.user.id === me.id && vacation.status !== "DECLINED")
+            setMyVacations(mine)
+        }
+
+        // eslint-disable-next-line
+    }, [vacations])
+
+    const onSuccessDaysRemaining = (days: number) => {
+        setDaysRemaining(days)
+    }
+
+    const renderVacationsOnCalendar = () => {
+        return vacations.map((vacation) => {
+            return {
+                id: vacation.id,
+                start: vacation.start,
+                end: vacation.end,
+                title: vacation.status,
+                extendedProps: vacation.user,
+            } as CalendarEvent
+        })
+    }
+
+    const renderRequests = () => {
+        if (myVacations.length > 0) {
+            return myVacations.map((vacation) => {
+                const subtitle = vacation.status === "APPROVED" ? "Approved Leave" : "Requested Leave"
+                return (
+                    <div>
+                        <Subtitle>{subtitle}</Subtitle>
+                        {vacationInfo(vacation)}
+                        {vacation.status === "PENDING" && <span className={styles.status}>Awaiting approval</span>}
+                    </div>
+                )
+            })
+        }
+        return <span className={styles.status}>No active or requested vacations</span>
+    }
+
+    const vacationInfo = (vacation: VacationRequestReviewType) => {
+        return (
+            <div>
+                <Item bold label="Days requested" children={vacation.daysRequested} />
+                <Item bold label="From" children={formatDate(vacation.start)} />
+                <Item bold label="To" children={formatDate(vacation.end)} />
+            </div>
+        )
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.calendar}>
+                <CalendarCore events={renderVacationsOnCalendar()} />
+            </div>
+            <Card>
+                {me && (
+                    <>
+                        <Subtitle>{`${me.firstName} ${me.lastName}`}</Subtitle>
+                        <Item bold label="Days remaining" children={daysRemaining} />
+
+                        <div className={styles.content}>{renderRequests()}</div>
+                    </>
+                )}
+            </Card>
+        </div>
+    )
+}
+export default CalendarMe
