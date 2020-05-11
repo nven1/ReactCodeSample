3773640563447923294686dbbd546fa7e0e81787
nven1 <neven.zdelar@gmail.com>
2020-05-11 16:48:27 +0200

    Event stylings utils file

---
+++ src/utils/eventStylings.ts
@@ -0,0 +1,2 @@
+export const holidayEventStyle = { backgroundColor: "#292961", borderColor: "#292961", rendering: "background" }
+export const previewEventStyle = { backgroundColor: "#ff0051", borderColor: "#ff0051" }
