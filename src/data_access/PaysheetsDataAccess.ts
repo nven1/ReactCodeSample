326deede54bebbc7c4937c83bd6fddb598b93f98
nven1 <neven.zdelar@gmail.com>
2020-05-19 10:46:36 +0200

    Paysheets data access, reducer, authHeader function updated

---
+++ src/data_access/PaysheetsDataAccess.ts
@@ -0,0 +1,56 @@
+import { Dispatch } from "redux"
+import axios from "axios"
+import Endpoints from "../environments/endpoints"
+import { AuthHeader } from "../utils/authHeader"
+import { getAllPaysheetsAction, PaysheetType, getMyPaysheetsAction } from "../reducers/PaysheetsReducer"
+import { showError } from "../utils/errorHandlers"
+
+const getAllPaysheets = (dispatch: Dispatch<any>) => () => {
+    axios
+        .get<Array<PaysheetType>>(`${Endpoints.apiEndpoint}/payment-list/all`, AuthHeader())
+        .then((response) => {
+            dispatch(getAllPaysheetsAction(response.data))
+        })
+        .catch((error) => {})
+}
+
+const getMyPaysheets = (dispatch: Dispatch<any>) => () => {
+    axios
+        .get<Array<PaysheetType>>(`${Endpoints.apiEndpoint}/payment-list`, AuthHeader())
+        .then((response) => {
+            dispatch(getMyPaysheetsAction(response.data))
+        })
+        .catch((error) => {})
+}
+
+const downloadPDF = (dispatch: Dispatch<any>) => (paysheetId: number) => {
+    axios
+        .get(`${Endpoints.apiEndpoint}/payment-list/${paysheetId}`, { ...AuthHeader(), responseType: "blob" })
+        .then((response) => {
+            const url = URL.createObjectURL(response.data)
+            window.open(url, "blank")
+        })
+        .catch((error) => {
+            showError(dispatch)(error)
+            console.log(error.response)
+        })
+}
+
+const uploadPDF = (dispatch: Dispatch<any>) => (paysheet: any, onSuccess: () => void) => {
+    axios
+        .post(`${Endpoints.apiEndpoint}/payment-list/`, paysheet, AuthHeader(true))
+        .then((response) => {
+            onSuccess()
+        })
+        .catch((error) => {
+            showError(dispatch)(error)
+            console.log(error.response)
+        })
+}
+
+export default {
+    getAllPaysheets,
+    getMyPaysheets,
+    downloadPDF,
+    uploadPDF,
+}
--- src/utils/authHeader.ts
+++ src/utils/authHeader.ts
@@ -1,8 +1,15 @@
 import { Token } from "./storageKeys"
 
-export const AuthHeader = () => {
+export const AuthHeader = (formData?: boolean) => {
     const token = localStorage.getItem(Token)
-
+    if (formData) {
+        return {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "content-type": "multipart/form-data",
+            },
+        }
+    }
     return {
         headers: {
             Authorization: `Bearer ${token}`,
