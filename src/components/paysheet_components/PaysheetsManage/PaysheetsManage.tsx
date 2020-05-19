dbc0d8b51e689a88776e66f419e5c5b3f7cb1c33
nven1 <neven.zdelar@gmail.com>
2020-05-19 10:53:41 +0200

    Manage paysheets

---
+++ src/components/paysheet_components/PaysheetsManage/PaysheetsManage.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react"
+import styles from "./PaysheetsManage.module.scss"
+import { useSelector, useDispatch } from "react-redux"
+import { selectUsers } from "../../../reducers/UserReducer"
+import StaffOverview from "../../staff_components/StaffOverview/StaffOverview"
+import { useParams } from "react-router"
+import UserPaysheets from "../UserPaysheets/UserPaysheets"
+import { selectAllPaysheets } from "../../../reducers/PaysheetsReducer"
+import PaysheetsDataAccess from "../../../data_access/PaysheetsDataAccess"
+
+interface PaysheetsManageProps {}
+
+const PaysheetsManage: React.FC<PaysheetsManageProps> = (props) => {
+    const dispatch = useDispatch()
+
+    const users = useSelector(selectUsers)
+    const paysheets = useSelector(selectAllPaysheets)
+
+    const { id } = useParams()
+
+    useEffect(() => {
+        PaysheetsDataAccess.getAllPaysheets(dispatch)()
+
+        // eslint-disable-next-line
+    }, [])
+
+    const getPaysheetsById = () => {
+        return paysheets.filter((paysheet) => paysheet.userId.toString() === id)
+    }
+
+    const getUserName = () => {
+        if (id) {
+            const user = users.filter((user) => user.id.toString() === id)[0]
+            if (user) {
+                return `${user.firstName} ${user.lastName}`
+            }
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <StaffOverview users={users} />
+            {id && <UserPaysheets paysheets={getPaysheetsById()} title={getUserName()} />}
+        </div>
+    )
+}
+export default PaysheetsManage
