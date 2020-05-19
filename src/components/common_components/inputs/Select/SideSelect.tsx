6547e377a7971a55e7c519b5c8167e644d730f7f
nven1 <neven.zdelar@gmail.com>
2020-05-19 10:42:34 +0200

    Side select element

---
+++ src/components/common_components/inputs/Select/SideSelect.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react"
+import styles from "./SideSelect.module.scss"
+import classNames from "classnames"
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io"
+
+export interface SideSelectOptionType {
+    label: string
+    value: string
+}
+
+interface SideSelectProps {
+    options: Array<SideSelectOptionType>
+    selectedIndex?: number
+    label: string
+    cardFit?: boolean
+    onSelect: (data: any) => void
+}
+
+const SideSelect: React.FC<SideSelectProps> = (props) => {
+    const classProps = classNames(styles.container, {
+        [styles.cardFit]: props.cardFit,
+    })
+
+    const [selectedIndex, setselectedIndex] = useState<number>(props.selectedIndex ?? 0)
+
+    useEffect(() => {
+        if (props.selectedIndex !== undefined) {
+            setselectedIndex(Number(props.selectedIndex))
+        }
+
+        // eslint-disable-next-line
+    }, [props.selectedIndex])
+
+    useEffect(() => {
+        props.onSelect(props.options[selectedIndex].value)
+
+        // eslint-disable-next-line
+    }, [selectedIndex])
+
+    const isNotFirst = selectedIndex > 0
+    const isNotLast = selectedIndex < props.options.length - 1
+
+    const handleNext = (e: any) => {
+        e.preventDefault()
+        e.stopPropagation()
+        if (isNotLast) {
+            setselectedIndex(selectedIndex + 1)
+        }
+    }
+
+    const handlePrevious = (e: any) => {
+        e.preventDefault()
+        e.stopPropagation()
+        if (isNotFirst) {
+            setselectedIndex(selectedIndex - 1)
+        }
+    }
+
+    const prevent = (e: any) => {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+
+    return (
+        <div className={classProps} onClick={prevent}>
+            <div className={styles.sideSelect}>
+                <button disabled={!isNotFirst} onClick={handlePrevious}>
+                    <IoIosArrowBack />
+                </button>
+                <span>{props.options[selectedIndex].label}</span>
+                <button disabled={!isNotLast} onClick={handleNext}>
+                    <IoIosArrowForward />
+                </button>
+            </div>
+        </div>
+    )
+}
+export default SideSelect
