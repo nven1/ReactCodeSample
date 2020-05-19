33d8f9bfd5d0b68676dd18125bfc0aab58116c44
nven1 <neven.zdelar@gmail.com>
2020-05-19 11:02:08 +0200

    Staff overview adjusted for PaysheetsManage

--- src/components/staff_components/StaffOverview/StaffOverview.tsx
+++ src/components/staff_components/StaffOverview/StaffOverview.tsx
@@ -3,7 +3,7 @@ import styles from "./StaffOverview.module.scss"
 import Card from "../../common_components/containers/Card/Card"
 import { UserType } from "../../../types/UserTypes"
 import Title from "../../common_components/text/Title/Title"
-import { Link } from "react-router-dom"
+import { Link, useRouteMatch } from "react-router-dom"
 import Endpoints from "../../../environments/endpoints"
 import { goTo } from "../../../utils/navHelpers"
 
@@ -11,15 +11,24 @@ interface StaffOverviewProps {
     users: Array<UserType>
 }
 
-const URL = Endpoints.appEndpoints.staff
+const URL_STAFF = Endpoints.appEndpoints.staff
+const URL_PAYSHEETS = `${Endpoints.appEndpoints.paysheets}/manage`
 
 const StaffOverview: React.FC<StaffOverviewProps> = (props) => {
+    const isPaysheets = useRouteMatch({
+        path: URL_PAYSHEETS,
+    })
+
     return (
         <Card>
             <Title>Staff</Title>
             <ul className={styles.content}>
                 {props.users.map((user) => (
-                    <Link to={goTo(URL, user.id.toString())} key={user.id} className={styles.user}>
+                    <Link
+                        to={goTo(isPaysheets ? URL_PAYSHEETS : URL_STAFF, user.id.toString())}
+                        key={user.id}
+                        className={styles.user}
+                    >
                         {`${user.firstName} ${user.lastName}`}
                     </Link>
                 ))}
