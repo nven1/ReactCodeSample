5643640536661cbce85f0a2c1a57df8bf48ec34f
nven1 <neven.zdelar@gmail.com>
2020-05-19 13:06:36 +0200

    MR fix, now creates link though props

--- src/components/staff_components/StaffOverview/StaffOverview.tsx
+++ src/components/staff_components/StaffOverview/StaffOverview.tsx
@@ -3,32 +3,21 @@ import styles from "./StaffOverview.module.scss"
 import Card from "../../common_components/containers/Card/Card"
 import { UserType } from "../../../types/UserTypes"
 import Title from "../../common_components/text/Title/Title"
-import { Link, useRouteMatch } from "react-router-dom"
-import Endpoints from "../../../environments/endpoints"
+import { Link } from "react-router-dom"
 import { goTo } from "../../../utils/navHelpers"
 
 interface StaffOverviewProps {
     users: Array<UserType>
+    linkTo: string
 }
 
-const URL_STAFF = Endpoints.appEndpoints.staff
-const URL_PAYSHEETS = `${Endpoints.appEndpoints.paysheets}/manage`
-
 const StaffOverview: React.FC<StaffOverviewProps> = (props) => {
-    const isPaysheets = useRouteMatch({
-        path: URL_PAYSHEETS,
-    })
-
     return (
         <Card>
             <Title>Staff</Title>
             <ul className={styles.content}>
                 {props.users.map((user) => (
-                    <Link
-                        to={goTo(isPaysheets ? URL_PAYSHEETS : URL_STAFF, user.id.toString())}
-                        key={user.id}
-                        className={styles.user}
-                    >
+                    <Link to={goTo(props.linkTo, user.id.toString())} key={user.id} className={styles.user}>
                         {`${user.firstName} ${user.lastName}`}
                     </Link>
                 ))}
